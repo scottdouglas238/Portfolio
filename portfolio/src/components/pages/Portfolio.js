ba8c543e702c8253367a1689b3e33668666cb7cb
Scott <scottdouglas238@gmail.com>
2020-10-05 22:24:48 -0700

    Some Portfolio Links added

--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -1,22 +1,62 @@
 import React from "react";
+import "../styles/portfolio.css"
 
 function Blog() {
   return (
-    <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-        turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-        Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-        nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi
-        justo laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad
-        litora torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum
-        tellus eget lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec
-        placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque
-        vehicula sit amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus
-        non ullamcorper iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
-    </div>
+    <>
+     <div className = "container">
+      <div className = "row">
+       <div className = "col-sm-12 col-md-6 col-lg-6">
+        <div className = "w-100 card bg-dark text-white">
+       <h2>Portfolio</h2>
+        <hr></hr>
+         <div className = "row">
+          <div className = "col-md-6">
+           <p>How to contact me:</p>
+           <p>scottdouglas238@gmail.com</p>
+          </div>
+           <div className = "col-md-6">
+            <p>My GitHub Profile Page:</p>
+            <a className = "ancorTag" target = "_blank" href = "https://github.com/scottdouglas238">github.com/scottdouglas238</a>
+           </div>
+         </div>
+         <hr></hr>
+         <div className = "row">
+          <div className = "col-md-6">
+           <p>Phone Number:</p>
+           <p>602-505-2339</p>
+          </div>
+           <div className = "col-md-6">
+            <p>My LinkedIn Profile:</p>
+            <a className = "ancorTag" target = "_blank" href = "https://www.linkedin.com/in/scott-douglas-8482641ab/">linkedin.com/scott-douglas</a>
+           </div>
+         </div>
+         <hr></hr>
+         <div className = "row">
+          <div className = "col-md-6">
+           <p>My Resume:</p>
+           <a className = "ancorTag" target = "_blank" href = "https://drive.google.com/file/d/19o2VPyN_o_Y4wTTaf8B30ka-i4KIT522/view?usp=sharing">Scott Douglas Resume</a>
+          </div>
+         </div>
+        </div>
+       </div>
+        <div className = "col-sm-12 col-md-6 col-lg-6">
+         <div className = "w-100 card bg-dark text-white">
+         <div className = "col-md-6">
+          <h2>Projects</h2>
+          <hr></hr>
+           <div className = "row">
+            <div className = "col-md-6">
+             
+            </div>
+           </div>
+          </div>
+        </div>
+        </div>
+      </div>
+     </div>
+      
+    </>
   );
 }
 
