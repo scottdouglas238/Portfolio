9eea29f24505c705b10a2d0fd10b9de501966550
Scott <scottdouglas238@gmail.com>
2020-10-05 21:19:32 -0700

    Contact Page added

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,22 +1,30 @@
 import React from "react";
+import "../styles/contact.css"
 import { Link, Route } from "react-router-dom";
 
 
 function Contact(props) {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis molestie urna.
-        Aliquam semper ultrices varius. Aliquam faucibus sit amet magna a ultrices. Aenean
-        pellentesque placerat lacus imperdiet efficitur. In felis nisl, luctus non ante euismod,
-        tincidunt bibendum mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque nec
-        metus vestibulum, egestas massa eu, sollicitudin ipsum. Nulla facilisi. Sed ut erat ligula.
-        Nam tincidunt nunc in nibh dictum ullamcorper. Class aptent taciti sociosqu ad litora
-        torquent per conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus.
-        Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div className="container">
+      <div id = "contactCard" className="w-75 p-3 card bg-dark text-white">
+        <h2>Contact</h2>
+        <hr></hr>
+         <form>
+         <div class="form-group">
+                <label for="exampleFormControlInput1">Name</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Name Here"></input>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Email Adress Here"></input>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="message" rows="3"></textarea>
+            </div>
+         </form>
+         <button id = "contactBtn" type="button" class="btn btn-info">Submit</button> 
+      </div>
     </div>
   );
 }
