4e692a2de81e038613607e142ce6719267188cc1
Scott <scottdouglas238@gmail.com>
2020-10-07 20:10:23 -0700

    Updates

---
+++ precache-manifest.6f385126ed6826a56f103c0e94279b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ec0a863aecde44f52d4b8e4708ba64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d31f2d06d048d3d50d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "44abdb4c5bb654502611",
+    "url": "/static/css/main.f3348369.chunk.css"
+  },
+  {
+    "revision": "e6d31f2d06d048d3d50d",
+    "url": "/static/js/2.4bd55e5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4bd55e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44abdb4c5bb654502611",
+    "url": "/static/js/main.614cacdc.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
